f3383c1426e36b7353f6e54ac64ad94368e15daf
anothernewbie UwU <namabaru786@gmail.com>
2025-06-25 06:51:28 +0000

    KernelSU: revert some unnecessary commit

--- build.sh
+++ build.sh
@@ -62,7 +62,3 @@ python3 $KERNEL_DIR/mkdtboimg.py create $AK3_DIR/dtbo.img $KERNEL_DIR/out/arch/a
 make
 cd $KERNEL_DIR
 rm -rf out/arch/arm64/boot
-
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
